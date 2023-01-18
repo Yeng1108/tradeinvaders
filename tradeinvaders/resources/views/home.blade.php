54c3a709a8828c9e3e36926b093804dd44999668
Jovenil Medina <106884817+Yeng1108@users.noreply.github.com>
2023-01-19 00:41:02 +0800

    setup for auth

--- tradeinvaders/resources/views/home.blade.php
+++ tradeinvaders/resources/views/home.blade.php
@@ -4,17 +4,32 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            asd
+            <div class=""></div>asd
         </div>
         <div class="col-9 p-5">
             <div class="d-flex align-items-center pb-4">
-                <h1>TradeInvaders</h1>
+                <h1>{{$user -> username}}</h1>
             </div> 
             <div class="d-flex">
                 <div class="pr-3"><strong>127k</strong>&nbspPost</div>
                 <div class="pr-3"><strong>24k</strong>&nbspFollowers</div>
                 <div class="pr-3"><strong>300</strong>&nbspFollowing</div>
             </div>
+            <div class="pt-4 font-weight: bold">Jovenil Medina</div>
+            <div class="div">This is a sample pogi description</div>
+            <div class="div">jovenilmedina.com</div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4">
+            <img src="img/post1.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="img/post3.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="img/post4.jpg" class="w-100">
         </div>
     </div>
 </div>
