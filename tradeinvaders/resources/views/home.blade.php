78e792d6f40ec38f504aeb7ba08d4fe72f65ea6e
Jovenil Medina <jovenilmedina08@gmail.com>
2023-01-18 18:55:59 +0800

    setup tradeinvaders

---
+++ tradeinvaders/resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            asd
+        </div>
+        <div class="col-9 p-5">
+            <div class="d-flex align-items-center pb-4">
+                <h1>TradeInvaders</h1>
+            </div> 
+            <div class="d-flex">
+                <div class="pr-3"><strong>127k</strong>&nbspPost</div>
+                <div class="pr-3"><strong>24k</strong>&nbspFollowers</div>
+                <div class="pr-3"><strong>300</strong>&nbspFollowing</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- tradeinvaders/resources/views/welcome.blade.php
+++ tradeinvaders/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Trade-Invaders
                 </div>
 
                 <div class="links">
