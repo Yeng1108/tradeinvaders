6573fd51f396f4bf62889c0c5815361b3b6a233e
Jovenil Medina <jovenilmedina08@gmail.com>
2023-02-10 17:36:49 +0800

    Create 2023_02_10_154948_create_customers_table.php

---
+++ tradeinvaders/database/migrations/2023_02_10_154948_create_customers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('CustomerName')->nullable();
+            $table->string('Contact')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('birthday')->nullable();
+            $table->string('address')->nullable();
+            $table->string('baranggay')->nullable();
+            $table->string('city')->nullable();
+            $table->string('province')->nullable();
+            $table->timestamps();
+
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
